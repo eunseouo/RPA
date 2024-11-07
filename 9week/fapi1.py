68e091f6e9d6c14fc9a7d17e620b43393f88f377
eunseo <shines3737@naver.com>
2024-11-07 12:08:23 +0900

    9주차 실습

---
+++ 9week/fapi1.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Form, File, UploadFile  # File로 수정
+import shutil
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/item")
+def read_item(item_id: int, name: str = None, age: int = 0):
+    return {"item_id": item_id, "name": name, "age": age}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    save_path = Path("static/uploads") / file.filename
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with save_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"filename": file.filename, "location": str(save_path)}
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000, log_level="info")
